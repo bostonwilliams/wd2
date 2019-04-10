61cef50417cc2b52cf751b302fc21cb273722ba2
bostonwilliams <47986742+bostonwilliams@users.noreply.github.com>
2019-04-10 13:11:48 -0400

    Update code.js

--- code.js
+++ code.js
@@ -2,8 +2,8 @@ $(document).ready(function() {
       $('.toggleNav').click(function() {
         $('#flex-nav ul').toggleClass('open');
       });
-      $('.toggleMain').click(function() {
-    $(this).addClass('shadow')
-    $(this).removeClass('shadow')
+      $('.toggleNav').click(function() {
+    $(this).addClass('shadow")
+    $('.shadow').removeClass('shadow')
       });       
     });
