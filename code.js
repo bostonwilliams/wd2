3dbe79f787f4b26a7c0e8731abd79fbc96ba8e34
bostonwilliams <47986742+bostonwilliams@users.noreply.github.com>
2019-04-17 13:43:17 -0400

    Update code.js

--- code.js
+++ code.js
@@ -2,6 +2,6 @@ $(document).ready(function() {
       $(".toggleNav").click(function() {
         $("#flex-nav ul").toggleClass("open")
             });
-      $(".phone").click(function() {
-        $(this).show();
+      $('.call').click(function() {
+        $('.phone').toggleClass('open');
     }); 
