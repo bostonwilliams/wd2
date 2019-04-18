ff98b89c310c3e37f286dded81b716b699f7efa1
bostonwilliams <47986742+bostonwilliams@users.noreply.github.com>
2019-04-18 12:15:34 -0400

    Update code.js

--- code.js
+++ code.js
@@ -1,11 +1,7 @@
 $(document).ready(function() {
       $('.toggleNav').click(function() {
         $('#flex-nav ul').toggleClass('open')
-            })
-       $('.call').on('click', () => {
-     $('.phone').toggleClass()
-    $('.call').toggleClass()
-  })  
+   
   $('.button').on('click', () => {
      $('.shadow').fadeOut('fast')
   })  
